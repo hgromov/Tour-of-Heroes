8e67230fef4a9b5747cc0034eb925941c8b17812
Herman Ostapenko <Herman_Ostapenko@epam.com>
2019-07-12 11:12:31 +0300

    second

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from '../../models/hero';
+import { HEROES } from "../../mock-heroes";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.scss"]
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
-
 }
